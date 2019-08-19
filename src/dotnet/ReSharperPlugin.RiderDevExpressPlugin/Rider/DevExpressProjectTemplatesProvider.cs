c07742eb4c9d7ae70bfcdcf77a3d86fbb592f00f
Nikita Lyubimov <lyubimov.nkt@gmail.com>
2019-08-19 21:31:51 +0300

    Check for installed DevExpress versions

--- src/dotnet/ReSharperPlugin.RiderDevExpressPlugin/Rider/DevExpressProjectTemplatesProvider.cs
+++ src/dotnet/ReSharperPlugin.RiderDevExpressPlugin/Rider/DevExpressProjectTemplatesProvider.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using JetBrains.Application;
+using JetBrains.Metadata.Reader.API;
+using JetBrains.Metadata.Utils;
 using JetBrains.ReSharper.Host.Features.ProjectModel.ProjectTemplates.DotNetTemplates;
-using JetBrains.Rider.Model;
 using JetBrains.Util;
 
 namespace ReSharperPlugin.RiderDevExpressPlugin.Rider
@@ -11,15 +12,34 @@ namespace ReSharperPlugin.RiderDevExpressPlugin.Rider
     {
         private readonly FileSystemPath myTemplatesDirectoryPath;
 
+        private readonly List<string> mySupportedVersions = new List<string>
+        {
+            "19.1"
+        };
+
         public DevExpressProjectTemplatesProvider()
         {
             var assemblyLocation = FileSystemPath.Parse(typeof(DevExpressProjectTemplatesProvider).Assembly.Location);
-            myTemplatesDirectoryPath = assemblyLocation.Parent/ "templates";
+            myTemplatesDirectoryPath = assemblyLocation.Parent / "templates";
         }
 
         public IReadOnlyCollection<FileSystemPath> GetSources(bool creatingSolution)
         {
-            return new[] {myTemplatesDirectoryPath / "19.1"};
+            var gacAssemblyResolver = DotNetFrameworkGacAssemblyResolver.CreateOnCurrentRuntimeGac(GacResolvePreferences.MatchSameOrNewer);
+
+            var templatesDirectoriesList = new List<FileSystemPath>();
+            foreach (var supportedVersion in mySupportedVersions)
+            {
+                var targetAssemblyName = AssemblyNameInfo.Parse($"DevExpress.Win.Projects.v{supportedVersion}.Design");
+                var assemblyPath = gacAssemblyResolver.TryResolveAssemblyPath(targetAssemblyName);
+                if (assemblyPath != null)
+                {
+                    templatesDirectoriesList.Add(myTemplatesDirectoryPath / supportedVersion);
+                }
+            }
+
+
+            return templatesDirectoriesList;
         }
     }
 }
